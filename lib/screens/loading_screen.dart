6ebe8711b9fe075f6935c04eceb8113d50ea2f5e
TheBigJae <johnbamigbola10@gmail.com>
2023-10-11 09:14:07 +0100

    fixed socket_patch error

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -11,6 +11,8 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double longtitude;
+  late double latitude;
   @override
   void initState() {
     super.initState();
@@ -20,11 +22,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longtitude);
+    latitude = location.latitude;
+    longtitude = location.longtitude;
+    await getData();
   }
-  void getData(){
-    get
+
+   getData() async {
+    http.Response response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=33&lon=139&appid=$apiKey'));
+        
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodeData = jsonDecode(data);
+      double temperature = decodeData['main']['temp'];
+      int condition = decodeData['weather'][0]['id'];
+      String cityName = decodeData['name'];
+
+      print(temperature);
+      print(condition);
+      print(cityName);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
